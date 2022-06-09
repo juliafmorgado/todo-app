df496bec8e1b11cd96254b78046d089fdd9e2d68
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-09 13:47:20 -0400

    added sendFile method

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
 app.listen(3000, function() {
     console.log('listening');
 })
