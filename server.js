70b72c27ab0f70a825e6596214c0cb51b208efec
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-16 14:04:38 -0400

    added express handlers into mongoClient then call

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://kaka:3887@cluster0.eomzavh.mongodb.net/?retryWrites=true&w=majority'
 
 
-app.use(bodyParser.urlencoded({ extended: true}))
+
+
+MongoClient.connect(connectionString)
+    .then(client => {
+    console.log('connected to database')
+    const db = client.db('to-do-app')
+    app.use(bodyParser.urlencoded({ extended: true}))
 
 
 app.get('/', (req, res) => {
@@ -22,9 +28,5 @@ app.listen(3000, function() {
     console.log('listening');
 })
 
-MongoClient.connect(connectionString)
-    .then(client => {
-    console.log('connected to database')
-    const db = client.db('to-do-app')
 })
     .catch(error => console.error(error))
