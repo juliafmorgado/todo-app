e4ec3000f6407e21cadcba31a96bc64298a7e565
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-16 15:16:07 -0400

    convert data with json stringify

--- public/main.js
+++ public/main.js
@@ -3,6 +3,9 @@ const update = document.querySelector('#update-button')
 update.addEventListener('click', _ => {
     fetch('/tasks', {
         method: 'put',
-        headers: { 'Content-Type': 'application/json'}
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            task: 'Do dishes'
+        })
     })
 })
