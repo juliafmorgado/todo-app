4aa4fe3dbee909e9ee519d7edf62105c8250b244
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-29 19:58:50 -0400

    trying to make delete request work

--- public/main.js
+++ public/main.js
@@ -1,42 +1,55 @@
-const deleteButton = document.querySelector('#delete-button')
+var trash = document.getElementsByClassName('fa-trash')
 
-Array.from(deleteText).forEach((element) => {
-    element.addEventListener('click', deleteTask)
-})
-
-
-deleteButton.addEventListener('click', _ => {
-    fetch('/tasks', {
-        method: 'delete',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            task: 'Groceries'
-        })
-    })
-})
-    .then(res => {
-        if(res.ok) return res.json()
-    })
-    .then(data => {
-        window.location.reload()
-    })
-
-
-async function deleteTask(){
-    const sTask = this.parentNode.childNodes[1].innerText
-    try{
-        const res = await fetch('deleteTask', {
+Array.from(trash).forEach((element) => {
+    element.addEventListener('click', function(){
+        const taskS = this.parentNode.parentNode.childNodes[1].innerText
+        fetch('tasks', {
             method: 'delete',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-                'taskS': sTask
+                'task': taskS
             })
+        }).then(res => {
+            window.location.reload()
         })
-        const data = await res.json()
-        console.log(data)
-        location.reload()
-    }
-    catch(err){
-        console.log(err)
-    }
-}
+    })
+})
+
+
+// deleteButton.addEventListener('click', _ => {
+//     fetch('/tasks', {
+//         method: 'delete',
+//         headers: { 'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//             task: 'Groceries'
+//         })
+//     })
+// })
+//     .then(res => {
+//         if(res.ok) return res.json()
+//     })
+//     .then(data => {
+//         window.location.reload()
+//     })
+
+
+// async function deleteTask(){
+//     const sTask = this.parentNode.childNodes[1].innerText
+//     try{
+//         const res = await fetch('deleteTask', {
+//             method: 'delete',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//                 'taskS': sTask
+//             })
+//         })
+//         const data = await res.json()
+//         console.log(data)
+//         location.reload()
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://kaka:3887@cluster0.eomzavh.mongodb.net/?retryWrites=true&w=majority'
 
-
+var db, collection;
 
 
 MongoClient.connect(connectionString)
     .then(client => {
-    console.log('connected to database')
+    console.log(`Connected to Database`)
     const db = client.db('to-do-app')
     const tasksCollection = db.collection('tasks')
 
@@ -38,30 +38,30 @@ app.post('/tasks', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.put('/tasks', (req, res) => {
-    tasksCollection.findOneAndUpdate(
-        query,
-        {
-            $set: {
-                task: req.body.task
-            }
-        },
-        {
-            upsert: true
-        }
-    )
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => console.error(error))
-})
+// app.put('/tasks', (req, res) => {
+//     tasksCollection.findOneAndUpdate(
+//         query,
+//         {
+//             $set: {
+//                 task: req.body.task
+//             }
+//         },
+//         {
+//             upsert: true
+//         }
+//     )
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(error => console.error(error))
+// })
 
-app.delete('deleteTask', (req, res) => {
+app.delete('/tasks', (req, res) => {
     console.log(req)
-    db.collection('tasks').deleteOne({task: req.body.taskS})
+    tasksCollection.findOneAndDelete({task: req.body.task})
     .then(result => {
         console.log('Task deleted')
-        response.json('Task deleted')
+        res.send('Task deleted')
     })
     .catch(error => console.error)
 })
