875aa3726612e11489dc7acfacfc595287ea29f4
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-30 12:52:28 -0400

    cleaning main.js

--- public/main.js
+++ public/main.js
@@ -1,26 +1,6 @@
 var trash = document.getElementsByClassName('fa-trash')
 Array.from(trash).forEach(elem => elem.addEventListener('click', deleteOne))
 
-// Array.from(trash).forEach((element) => {
-//     element.addEventListener('click', function(){
-//         const taskS = this.parentNode.parentNode.childNodes[1].innerText
-//         fetch('tasks', {
-//             method: 'delete',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({
-//                 'task': taskS
-//             })
-//         }).then(res => {
-//             window.location.reload()
-//         })
-//     })
-// })
-
-// Array.from(trash).forEach((element) => element.addEventListener('click', function(){
-    
-// }))
 
 async function deleteOne(){
     const sTask = this.parentNode.childNodes[1].innerText
@@ -40,21 +20,4 @@ async function deleteOne(){
         console.log(err)
     }
 }
-
-// deleteButton.addEventListener('click', _ => {
-//     fetch('/tasks', {
-//         method: 'delete',
-//         headers: { 'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             task: 'Groceries'
-//         })
-//     })
-// })
-//     .then(res => {
-//         if(res.ok) return res.json()
-//     })
-//     .then(data => {
-//         window.location.reload()
-//     })
-
 
